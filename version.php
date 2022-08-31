4cd204a7fd0b13c30e782e25ccd4d4acc4cded94
Pierre-Olivier Vallès <pierre-olivier.valles@epfl.ch>
2022-08-31 10:25:52 +0000

    Update plugin version

--- version.php
+++ version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2020061505;
+$plugin->version   = 2022083101;
 $plugin->requires  = 2020060900;
 $plugin->component = 'assignsubmission_noto';
